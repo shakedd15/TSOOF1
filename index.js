ef7e7e00725b28e738b67e24280630ad52052f62
shakedd20 <shakedd15@gmail.com>
2019-08-21 00:28:41 +0300

    add soma check changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var count = 0 ;
+var dataObject;
+
 function encodeHTML(text) {
     return text.replace(/&/g, '&amp;')
         .replace(/"/g, '&quot;')
@@ -6,17 +9,80 @@ function encodeHTML(text) {
         .replace(/>/g, '&gt;')
 }
 
-function check() {
-    document.getElementById("second").checked = true;
-}
-
-function uncheck() {
-    document.getElementById("myCheck").checked = false;
+function check(os, name) {
+    document.getElementById(name).checked = os;
 }
 
 window.onload = function() {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://api.myjson.com/bins/jr6vz", false ); // false for synchronous request
-    xmlHttp.send( null );
-    var list = xmlHttp.responseText;
+    var HttpClient = function() {
+        this.get = function(aUrl, aCallback) {
+            var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.onreadystatechange = function() {
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseText);
+            }
+
+            anHttpRequest.open( "GET", aUrl, false );
+            anHttpRequest.send( null );
+        }
+    }
+    
+    var list;
+    var client = new HttpClient();
+    client.get('https://api.myjson.com/bins/11v7vr', function(response) {
+        list = response;
+    });
+    this.dataObject = JSON.parse(list);
+
+    createListPlease(list);
 };
+
+function createListPlease(dataJSON) {
+    var listItemString = $('#listItem').html();
+    Object.keys(this.dataObject).forEach(buildNewList);
+
+    function buildNewList(item) {
+        var listItem = $('<li style="color: white;\n' +
+            'font-family: cursive;\n' +
+            'font-size: 156%;">' + listItemString + '</li>');
+        var listItemTitle = $('.title', listItem);
+        var insert = "<input type=\"checkbox\" onclick=\"checking(this)\" id=\"" + item + "\">" + item;
+        listItemTitle.html(insert);
+        $('#dataList').append(listItem);
+        if(this.dataObject[item] == "true"){
+            check(this.dataObject[item], item);
+        }
+        count++;
+    }
+}
+
+function checking(el) {
+    Object.keys(this.dataObject).forEach(function(key) {
+        if(key == el.id){
+            if(this.dataObject[el.id] == "true"){
+                this.dataObject[el.id] = "false";
+            } else {
+                this.dataObject[el.id] = "true";
+            }
+        }
+    });
+}
+
+function saveChanges() {
+    $.ajax({
+        url:"https://api.myjson.com/bins/11v7vr",
+        type:"PUT",
+        data:JSON.stringify(this.dataObject),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data, textStatus, jqXHR){
+            window.alert("השמירה הצליחה מאמי")
+        }
+    });
+}
+
+
+// {
+//     "shaked": "true",
+//     "tsoof": "false"
+// }
